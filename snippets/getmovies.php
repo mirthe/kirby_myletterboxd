4ef46a68730f1398033508547d77b61e5caa04f7
Mirthe Valentijn <me@mirthe.org>
2025-10-09 20:51:48 +0200

    Add user agent to curl to prevent being blocked by CloudFlare

--- snippets/getmovies.php
+++ snippets/getmovies.php
@@ -7,6 +7,7 @@
         $ch = curl_init($feedurl);
         curl_setopt($ch, CURLOPT_URL, $feedurl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, $site->title());
 
         // letterboxd certificate expired.. :-(
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
