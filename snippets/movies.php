85d1eded1936873c06e555df2e4ea34e3afeccc7
Mirthe Valentijn <me@mirthe.org>
2026-05-22 12:54:12 +0200

    Hide circles when there's no rating given

--- snippets/movies.php
+++ snippets/movies.php
@@ -34,23 +34,25 @@
                 <?php if ($item->letterboxd_rewatch == 'Yes'): ?>
                     <span style="color: silver; float: right">Rewatch</span>
                 <?php endif ?>
-                
-                <span class="nowrap">
-                <?php for ($i = 1; $i <= $item->letterboxd_memberRating; $i++) {
-                    echo ' <i class="fa-solid fa-circle"></i>';
-                }
+                    
+                <?php if($item->letterboxd_memberRating > 0): ?>
+                    <span class="nowrap">
+                        <?php for ($i = 1; $i <= $item->letterboxd_memberRating; $i++) {
+                            echo ' <i class="fa-solid fa-circle"></i>';
+                        }
 
-                if ( (float)$item->letterboxd_memberRating !== round((int)$item->letterboxd_memberRating)) {
-                    echo ' <i class="fa-solid fa-circle-half-stroke"></i>';
-                    $aanvullen = round((int)$item->letterboxd_memberRating) + 1;
-                } else {
-                    $aanvullen = round((int)$item->letterboxd_memberRating);
-                }
+                        if ( (float)$item->letterboxd_memberRating !== round((int)$item->letterboxd_memberRating)) {
+                            echo ' <i class="fa-solid fa-circle-half-stroke"></i>';
+                            $aanvullen = round((int)$item->letterboxd_memberRating) + 1;
+                        } else {
+                            $aanvullen = round((int)$item->letterboxd_memberRating);
+                        }
 
-                for ($i = $aanvullen; $i < 5; $i++) {
-                    echo ' <i class="fa-regular fa-circle fffa-inactive"></i>';
-                } ?>
-                </span>
+                        for ($i = $aanvullen; $i < 5; $i++) {
+                            echo ' <i class="fa-regular fa-circle fffa-inactive"></i>';
+                        } ?>
+                    </span>
+                <?php endif ?>
             </p>
 
             <?php if(substr(trim($mijnreview), 0, 10) !== 'Watched on'): ?>
